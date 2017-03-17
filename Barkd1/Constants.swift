1f2c0836a412ebb63bbbb1167f0edb546f57c904
Brian Lee <brianjacoblee@gmail.com>
2017-03-17 10:09:55 -0400

    Initial Commit

---
+++ Barkd1/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Barkd1
+//
+//  Created by MacBook Air on 3/17/17.
+//  Copyright © 2017 LionsEye. All rights reserved.
+//
+
+import UIKit
+
+let KEY_UID: String = "uid"
+
+
---
+++ Barkd1/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  Barkd1
+//
+//  Created by MacBook Air on 3/17/17.
+//  Copyright © 2017 LionsEye. All rights reserved.
+//
+
+import UIKit
+
+struct DataService {
+    
+}
