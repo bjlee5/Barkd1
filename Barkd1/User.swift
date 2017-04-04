457dc8ec0067795ba68b05468c8895826579a74a
Brian Lee <brianjacoblee@gmail.com>
2017-04-03 21:47:29 -0400

    Commit prior to changing Users from struct to class in refactoring

--- Barkd1/User.swift
+++ Barkd1/User.swift
@@ -35,4 +35,73 @@ struct Users {
     
 }
 
+class User {
+    private var _username: String!
+    private var _email: String!
+    private var _bio: String!
+    private var _photoURL: String!
+    private var _uid: String!
+    private var _ref: FIRDatabaseReference!
+    private var _key: String!
+    
+    var username: String {
+        return _username
+    }
+    
+    var email: String {
+        return _email
+    }
+    
+    var bio: String {
+        return _bio
+    }
+    
+    var photoURL: String {
+        return _photoURL
+    }
+    
+    var uid: String {
+        return _uid
+    }
+    
+    var key: String {
+        return _key
+    }
+    
+    init(username: String, email: String, bio: String, photoURL: String, uid: String) {
+        self._username = username
+        self._email = email
+        self._bio = bio
+        self._photoURL = photoURL
+        self._uid = uid 
+    }
+    
+    init(postKey: String, postData: Dictionary<String, Any>) {
+        self._key = postKey
+        
+        if let username = postData["username"] as? String {
+            self._username = username
+        }
+        
+        if let email = postData["email"] as? String {
+            self._email = email
+        }
+        
+        if let bio = postData["bio"] as? String {
+            self._bio = bio
+        }
+        
+        if let photoURL = postData["photoURL"] as? String {
+            self._photoURL = photoURL
+        }
+        
+        if let uid = postData["uid"] as? String {
+            self._uid = uid
+        }
+                
+        _ref = DataService.ds.REF_POSTS.child(_key)
+    }
+
+    
+}
 
